c9d2ea78940a0e8395771d093ddb39fa35410968
SkyLined <berendjanwever@gmail.com>
2015-04-26 21:40:27 +0200

    Make mSettings and export, order alphanumeric

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var path = require("path");
 var sModuleName = path.basename(__dirname);
-module.exports.cServer = require("./cServer.js");
-module.exports.fConnect = require("./fConnect.js");
-module.exports.mErrorCodes = require("./mErrorCodes.js");
+module.exports.mErrorCodes = require("./mErrorCodes");
+module.exports.fConnect = require("./fConnect");
+module.exports.cServer = require("./cServer");
+module.exports.mSettings = require(("./mSettings");
