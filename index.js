fbbeabad3cb20843a054feea115233490c101a7b
SkyLined <berendjanwever@gmail.com>
2015-04-28 10:53:41 +0200

    Alphabetize index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var path = require("path");
 var sModuleName = path.basename(__dirname);
-module.exports.dxErrorCodes = require("./dxErrorCodes");
 module.exports.fConnect = require("./fConnect");
+module.exports.dxErrorCodes = require("./dxErrorCodes");
 module.exports.cServer = require("./cServer");
 module.exports.dxSettings = require("./dxSettings");
